1ba889a9343211902fae2ca095e9e739ef7c2674
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-09-18 21:19:59 +0500

    Typescript practice

---
+++ types.js
@@ -0,0 +1,6 @@
+var applesCount = 5;
+var speed = 'fast';
+var hasName = true;
+var nothingMuch = null;
+var nothing = undefined;
+var now = new Date();
---
+++ types.ts
@@ -0,0 +1,23 @@
+let applesCount: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+let now: Date = new Date();
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+class Car {
+
+};
+let car: Car = new Car();
+let point: { x: number; y: number } = {
+  x: 30,
+  y: 47
+};
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+};
+const logNumberImproved = (i: number): void => {
+  console.log(i)
+};
